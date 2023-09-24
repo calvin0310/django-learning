67e61a552b34f58bd9e21e6f67425e1b682926a3
calvin0310 <s3101990@hotmail.com>
2023-09-24 18:00:41 +0800

    bootstrap

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
+import logging
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post is not None:
+            return render(request, 'post.html', locals())
+
+    except Exception as e:
+        logging.warning(e)
+        return redirect('/')
--- mblog/mblog/urls.py
+++ mblog/mblog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainsite.views import homepage, showpost
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage),
+    path('post/<slug:slug>', showpost),
 ]
